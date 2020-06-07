9af1b474f10b7749ba798978ab7a80a858d7b30e
Ekrem Bel <ekrem.bel1@gmail.com>
2020-06-07 14:39:21 -0500

    Done now

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Write your JavaScript code here!
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
 window.addEventListener("load", function() {
    
    let userInput = prompt("Where would you like to go? \n\ \n\ Type 0 for Tatooine \n\ Type 1 for Pern \n\ Type 2 for Saturn \n\ Type 3 for Mars \n\ Type 4 for K2-18b \n\ Type 5 for Jupiter \n\ \n\ Good luck!");
